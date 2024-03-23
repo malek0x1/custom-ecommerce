ae642e40224277cff266ea16309b95d218feede4
null <malek0x1@gmail.com>
2024-03-23 05:24:14 +0200

    nhance orde rpage

--- src/pages/orders.jsx
+++ src/pages/orders.jsx
@@ -2,26 +2,40 @@ import {
     Table,
     TableBody,
     TableCaption,
-    TableCell,
-    TableFooter,
-    TableHead,
+    TableCell, TableHead,
     TableHeader,
-    TableRow,
+    TableRow
 } from "@/components/ui/table"
 import Layout from "@/components/Layout"
-import { invoices } from "@/lib/data"
 import { useEffect, useState } from "react"
+import commerce from "../lib/commerce"
+import Spinner from "@/components/Spinner"
 
 const Orders = () => {
     const [ordersData, setOrdersData] = useState([])
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        // commerce.customer.getOrders('cstmr_K1YDR2qy29Qem6').then((res) => {
-        //     console.log(res);
-        //     setOrdersData(res)
-        //     setIsLoading(false)
-        // });
+
+        commerce.customer.about().then(x => {
+            commerce.customer.getOrders(x.id).then(data => {
+                if (data.data) {
+
+                    const orderInfo = data.data.map(order => {
+                        return {
+                            id: order.id,
+                            total: order.order_value.formatted_with_symbol,
+                            status_payment: order.status_payment || "123",
+                        }
+                    })
+                    setIsLoading(false)
+                    setOrdersData(orderInfo)
+                }
+            })
+
+        }).catch(e => {
+            console.log(e);
+        })
     }, [])
     return (
         <Layout
@@ -34,33 +48,30 @@ const Orders = () => {
                 <h2 className="uppercase text-2xl tracking-wide">My Orders</h2>
 
 
-                <Table>
-                    <TableCaption>A list of your recent invoices.</TableCaption>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="w-[100px]">Invoice</TableHead>
-                            <TableHead>Status</TableHead>
-                            <TableHead>Method</TableHead>
-                            <TableHead className="text-right">Amount</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {invoices.map((invoice) => (
-                            <TableRow key={invoice.invoice}>
-                                <TableCell className="font-medium">{invoice.invoice}</TableCell>
-                                <TableCell>{invoice.paymentStatus}</TableCell>
-                                <TableCell>{invoice.paymentMethod}</TableCell>
-                                <TableCell className="text-right">{invoice.totalAmount}</TableCell>
+                {!isLoading ? (
+                    <Table>
+                        <TableCaption>A list of your recent invoices.</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead >orderID</TableHead>
+                                <TableHead>Status</TableHead>
+                                <TableHead>Total</TableHead>
                             </TableRow>
-                        ))}
-                    </TableBody>
-                    <TableFooter>
-                        <TableRow>
-                            <TableCell colSpan={3}>Total</TableCell>
-                            <TableCell className="text-right">$2,500.00</TableCell>
-                        </TableRow>
-                    </TableFooter>
-                </Table>
+                        </TableHeader>
+                        <TableBody>
+                            {ordersData.map((invoice) => (
+                                <TableRow key={invoice.id}>
+                                    <TableCell className="font-medium">{invoice.id}</TableCell>
+                                    <TableCell>{invoice.status_payment}</TableCell>
+                                    <TableCell>{invoice.total}</TableCell>
+                                </TableRow>
+                            ))
+                            }
+                        </TableBody>
+                    </Table>
+                ) : (
+                    <Spinner color="black" />
+                )}
 
             </div>
         </Layout>
@@ -68,3 +79,9 @@ const Orders = () => {
 }
 
 export default Orders
+{/* <TableFooter>
+    <TableRow>
+        <TableCell colSpan={3}>Total</TableCell>
+        <TableCell className="text-right">$2,500.00</TableCell>
+    </TableRow>
+</TableFooter> */}
