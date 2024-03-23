59699f05ec02adb412853d3a68994b939118fedd
null <malek0x1@gmail.com>
2024-03-23 04:20:36 +0200

    collection page done

--- src/pages/collection/[collection].jsx
+++ src/pages/collection/[collection].jsx
@@ -34,7 +34,6 @@ const Collection = () => {
 
     const fetchProducts = async (pageNumber) => {
         try {
-            alert(`FETCHED ${pageNumber}`)
             const addFilters = chosenFilter.name ? { sortBy: chosenFilter.sortBy, sortDirection: chosenFilter.sortOrder } : {};
             const { data, meta } = await commerce.products.list({
                 category_slug: [router.query.collection],
