304edf7a9f604079e072209a8ce318226ebb1f2c
null <malek0x1@gmail.com>
2024-03-25 14:53:01 +0200

    revert

--- src/components/Picture/index.jsx
+++ src/components/Picture/index.jsx
@@ -1,4 +1,4 @@
-import Image, { getImageProps } from 'next/image'
+import { getImageProps } from 'next/image'
 
 export default function HeroPicture({ mobileSrc, desktopSrc }) {
     const common = { alt: 'Theme Example', fill: true }
@@ -10,11 +10,10 @@ export default function HeroPicture({ mobileSrc, desktopSrc }) {
     } = getImageProps({ ...common, src: mobileSrc, priority: true, fetchPriority: "high", loading: "eager" })
 
     return (
-        <Image src={mobileSrc} alt="" fill className='w-full h-full object-cover' />
-        // <picture>
-        //     <source media="(min-width: 900px)" srcSet={deskTop} />
-        //     <source media="(min-width: 300px)" srcSet={mobile} />
-        //     <img {...rest} className='w-full object-cover' />
-        // </picture>
+        <picture>
+            <source media="(min-width: 900px)" srcSet={deskTop} />
+            <source media="(min-width: 300px)" srcSet={mobile} />
+            <img {...rest} className='w-full object-cover' />
+        </picture>
     )
 }
