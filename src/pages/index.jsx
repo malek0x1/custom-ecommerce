4fab44b3c3e150da75bb864b2b225d6c20ca7b27
null <malek0x1@gmail.com>
2024-03-25 12:48:18 +0200

    isloading to false exp4

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,4 @@
-import dynamic from 'next/dynamic';
 import Layout from "@/components/Layout";
-import { CiShoppingBasket } from "react-icons/ci";
 import { useEffect, useState } from 'react';
 import { getPageBySlug } from '@/lib/helpers';
 import { Module } from '@/components/Modules';
@@ -8,11 +6,6 @@ import Skeleton from 'react-loading-skeleton';
 import HeroSkeleton from '@/components/Hero/HeroSkeleton';
 import SkeletonCard from '@/components/Card/SkeletonCard';
 import { Carousel, CarouselContent, CarouselItem } from '@/components/ui/carousel';
-
-const Hero = dynamic(() => import("@/components/Hero"));
-const ImageWithDescription = dynamic(() => import("@/components/ImageWithDescription"));
-const MainCategory = dynamic(() => import("@/components/MainCategory"));
-const ProductsCarousel = dynamic(() => import("@/components/ProductsCarousel"));
 
 export default function Home() {
   const [isFullLoading, setIsFullLoading] = useState(true)
@@ -26,7 +19,7 @@ export default function Home() {
       if (res.modules && res.modules.length > 0) {
         setPageData(res)
       }
-      // setIsFullLoading(false)
+      setIsFullLoading(false)
     }
     handlePage()
   }, [])
@@ -64,33 +57,6 @@ export default function Home() {
             <Module key={key} module={module} />
           ))}
       </div>
-
-
-      <>
-        {/* <Grid collection="apple" />
-      <HeroSkeleton />
-      <Hero />
-      <div className="">
-      <ImageWithDescription />
-      </div>
-      <ProductsCarousel collection="apple" />
-      
-      <ProductsCarousel collection="laptops" />
-      <div className="flex flex-wrap sm:flex-nowrap gap-4 justify-between sm:container w-full items-center">
-      <MainCategory />
-      <MainCategory />
-      </div>
-      <div className="flex flex-col bg-gray-50 sm:flex-row justify-between items-center gap-10 py-10">
-      {[1, 2, 3, 4].map(item => (
-        <div key={item} className="flex-1 text-center flex px-3 justify-center items-center gap-3 flex-col">
-        <CiShoppingBasket size={40} color="black" />
-        <p className="text-lg uppercase">Free Shipping</p>
-        <p className="text-xs text-gray-800 w-10/12" >We Offer Free shipping on all Us orders and all internatiols orders over 150$.</p>
-        </div>
-        ))}
-        </div> 
-      */}
-      </>
     </Layout>
   );
 }
