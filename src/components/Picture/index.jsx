ac3b7956b5213927deb7b0ec7d53ba29ec0833e5
null <malek0x1@gmail.com>
2024-03-25 14:50:45 +0200

    use next image

--- src/components/Picture/index.jsx
+++ src/components/Picture/index.jsx
@@ -1,4 +1,4 @@
-import { getImageProps } from 'next/image'
+import Image, { getImageProps } from 'next/image'
 
 export default function HeroPicture({ mobileSrc, desktopSrc }) {
     const common = { alt: 'Theme Example', fill: true }
@@ -10,10 +10,11 @@ export default function HeroPicture({ mobileSrc, desktopSrc }) {
     } = getImageProps({ ...common, src: mobileSrc, priority: true, fetchPriority: "high", loading: "eager" })
 
     return (
-        <picture>
-            <source media="(min-width: 900px)" srcSet={deskTop} />
-            <source media="(min-width: 300px)" srcSet={mobile} />
-            <img {...rest} className='w-full object-cover' />
-        </picture>
+        <Image src={mobileSrc} alt="" fill className='w-full h-full object-cover' />
+        // <picture>
+        //     <source media="(min-width: 900px)" srcSet={deskTop} />
+        //     <source media="(min-width: 300px)" srcSet={mobile} />
+        //     <img {...rest} className='w-full object-cover' />
+        // </picture>
     )
 }
