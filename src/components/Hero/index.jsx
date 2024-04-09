4f6bc2bf56ae586a1eb0e225dfc8bdf495367086
null <malek0x1@gmail.com>
2024-04-09 05:36:54 +0300

    animate hero

--- src/components/Hero/index.jsx
+++ src/components/Hero/index.jsx
@@ -2,6 +2,7 @@
 import { urlFor } from '@/lib/sanity'
 import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel'
 import HeroPicture from '../Picture'
+import AnimatedComponent from '../AnimationComponent'
 
 
 const Hero = ({ mobileImageUrl, desktopImageUrl }) => {
@@ -9,12 +10,15 @@ const Hero = ({ mobileImageUrl, desktopImageUrl }) => {
         <Carousel>
             <CarouselContent >
                 <CarouselItem className=" pl-0">
-                    <div className="bg-gray-300 w-full hero-wrapper">
-                        <HeroPicture
-                            desktopSrc={urlFor(desktopImageUrl.asset)}
-                            mobileSrc={urlFor(mobileImageUrl.asset)}
-                        />
-                    </div>
+                    <AnimatedComponent>
+
+                        <div className="bg-gray-300 w-full hero-wrapper">
+                            <HeroPicture
+                                desktopSrc={urlFor(desktopImageUrl.asset)}
+                                mobileSrc={urlFor(mobileImageUrl.asset)}
+                            />
+                        </div>
+                    </AnimatedComponent>
                 </CarouselItem>
 
             </CarouselContent>
